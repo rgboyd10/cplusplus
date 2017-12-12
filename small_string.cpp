1f7127af69b04fce79af308de0fcc36318c6a3c1
rgboyd10 <rgboyd10@gmail.com>
2017-12-11 20:26:24 -0500

    -

--- small_string.cpp
+++ small_string.cpp
@@ -6,4 +6,13 @@ using namspace std;
 int main()
 {
   string name;
+  cout << "Who are you ? ";
+  cin >> name;
+  string greeting = "Hello, " + name;
+  if(name == "Kate")
+    {
+      greeting += ", I know you!";
+    }
+  cout << greeting << endl;
+  return 0;
 }
