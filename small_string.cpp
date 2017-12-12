ca5b484aa87f202f02e02544c9e5cbc3d2f8982d
rgboyd10 <rgboyd10@gmail.com>
2017-12-11 20:52:41 -0500

    -

--- small_string.cpp
+++ small_string.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <string>
 
-using namspace std;
+using namespace std;
 
 int main()
 {
@@ -9,10 +9,21 @@ int main()
   cout << "Who are you ? ";
   cin >> name;
   string greeting = "Hello, " + name;
-  if(name == "Kate")
+  if(name == "Russell")
     {
       greeting += ", I know you!";
     }
   cout << greeting << endl;
+
+  int l = greeting.length();
+  cout << "\"" + greeting + "\" is "
+       << l << " characters long." << endl;
+  char space = greeting.find(' ');
+  string beginning = greeting.substr(space + 1);
+  cout << beginning << endl;
+  if(beginning == name)
+    {
+      cout << "expected result." << endl;
+    }
   return 0;
 }
---
+++ string_length_compare.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+  //declare variables
+  string guess1;
+  string guess2;
+
+  //requests input and reads into variables
+  cout << "Please enter the first string: \n";
+  cin >> guess1;
+  cout << "Please enter the second string: \n";
+  cin >> guess2;
+
+  if(guess1.length() > guess2.length())
+    {
+      cout << guess1 << " at length " << guess1.length() << " is longer than " << guess2 << " at length " << guess2.length() << ".\n";
+    }
+  else if (guess1.length() < guess2.length())
+    {
+      cout << guess1 << " at length " << guess1.length() << " is shorter than " << guess2 << " at length " << guess2.length() << ".\n";
+    }
+  else
+    {
+      cout << guess1 << " and " << guess2 << " are the same length at " << guess1.length() << ".\n";  
+    }
+  return 0;
+}
