2d1664ac097b3037f6425266f382fb61a9f553dd
rboyd@secureworks.com <rboyd@secureworks.com>
2017-12-12 14:43:15 -0500

    -

---
+++ simple_class/Account.cpp
@@ -0,0 +1,62 @@
+#include "Account.h"
+
+using namespace std;
+
+Account::Account():balance(0)//initialization syntax.  can only be used with constructors.
+{
+}
+
+vector<string> Account::Report()
+{
+  vector<string> report;
+  report.push_back("Balance is " + to_string(balance));
+  report.push_back("Transactions: ");
+
+  for (auto t:log)
+   {
+     report.push_back(t.Report());
+   }
+  report.push_back("------------------");
+
+  return report;
+}
+
+bool Account::Deposit(int amt)
+{
+  if(amt >= 0)
+    {
+      balance += amt;
+      log.push_back(Transaction(amt,"Deposit"));
+      return true;
+    }
+  else
+    {
+      return false;
+    }
+}
+
+bool Account::Withdraw(int amt)
+{
+  if (amt >= 0)
+    {
+    if (balance >= amt)
+      {
+        balance -= amt;
+        log.push_back(Transaction(amt,"Withdraw"));
+        return true;
+      }
+    else
+      {
+        return false;
+      }
+    }
+  else
+    {
+      return false;
+    }
+}
+/*
+int Account::getBalance()
+{
+  return balance;
+  }*/
