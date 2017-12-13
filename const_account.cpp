2ee7dec4a478a755371bcabd874023fa87d805a1
rgboyd10 <rgboyd10@gmail.com>
2017-12-12 23:17:28 -0500

    -

---
+++ const_account.cpp
@@ -0,0 +1,66 @@
+#include "Account.h"
+
+using namespace std;
+
+Account::Account():balance(0), limit(100)//initialization syntax.  can only be used with constructors.
+{
+}
+
+vector<string> Account::Report() const
+{
+  vector<string> report;
+  report.push_back("Balance is " + to_string(balance));
+  report.push_back("Transactions: ");
+
+  for (auto t:log)
+   {
+     report.push_back(t.Report());
+   }
+  report.push_back("------------------");
+
+  return report;
+}
+
+bool Account::Deposit(int amt)
+{
+  if(amt >= 0)
+    {
+      balance += amt;
+      log.push_back(Transaction(amt,"Deposit"));
+      balance -= 1;
+      log.push_back(Transaction(1,"Service Charge"));
+      return true;
+    }
+  else
+    {
+      return false;
+    }
+}
+
+bool Account::Withdraw(int amt)
+{
+  if (amt >= 0)
+    {
+    if (balance + limit >= amt)
+      {
+        balance -= amt;
+        log.push_back(Transaction(amt,"Withdraw"));
+	balance -= 1;
+	log.push_back(Transaction(1,"Service Charge"));
+        return true;
+      }
+    else
+      {
+        return false;
+      }
+    }
+  else
+    {
+      return false;
+    }
+}
+/*
+int Account::getBalance()
+{
+  return balance;
+  }*/
