a8918f005eeb46d0c6cd741915a2d81dba01613c
rboyd@secureworks.com <rboyd@secureworks.com>
2017-12-13 18:46:45 -0500

    -

---
+++ variables_and_types.cpp
@@ -0,0 +1,25 @@
+//Variables and types
+
+#include "stdafx.h"
+#include <iostream>
+
+int main()
+{
+  int i1 = 1;
+  std::cout << "i1 = " << i1 << std::endl;
+  int i2;
+  i2 = 2;
+  std::cout << "i2 = " << i2 << std::endl;
+  int i3(3);
+  std::cout << "i3 = " << i3 << std::endl;
+
+  double d1 = 2.2;
+  double d3 = i1;
+  int i4 = d1;
+  std::cout << "d1= " << d1 << std::endl;
+  std::cout << "d2 = " << d2 << std::endl;
+  std::cout << "i4 = " << i4 << std::endl;
+
+  char c1 = 'a';
+  
+}
