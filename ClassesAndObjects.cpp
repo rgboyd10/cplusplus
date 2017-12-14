b8e33acb02755350e1f43cf81d67e4b33f61bb51
rboyd@secureworks.com <rboyd@secureworks.com>
2017-12-14 16:06:28 -0500

    -

---
+++ ClassesAndObjects.cpp
@@ -0,0 +1,9 @@
+#include "Person.h"
+
+int main()
+{
+  Person p1;
+  Person p2;
+
+  return 0;
+}
---
+++ Person.h
@@ -0,0 +1,9 @@
+#include <string>
+
+class Person
+{
+ private:
+  std::string firstname;
+  std::string lastname;
+  int arbitrarynumber;
+};
