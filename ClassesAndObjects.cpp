7419cc7a098211e48edb78d9ee16a5134edad0e2
rgboyd10 <rgboyd10@gmail.com>
2017-12-17 12:05:03 -0500

    -

--- ClassesAndObjects.cpp
+++ ClassesAndObjects.cpp
@@ -1,10 +1,13 @@
-#include "Person.h"
+//#include "Person.h"
+#include "Tweeter.h"
+#include <iostream>
 
 int main()
 {
   Person p1("Kate", "Gregory", 123);
   {
-    Person p2("Someone", "Else", 456);
+    Tweeter t1("Someone", "Else", 456, "@whoever");
   }
+  std::cout << "after innermost block" <<  std::endl;
   return 0;
 }
