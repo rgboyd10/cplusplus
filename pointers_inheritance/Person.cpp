b42f9582c988c1f56bd05f58c2dc7adc775e806a
rgboyd10 <rgboyd10@gmail.com>
2018-01-03 21:13:32 -0500

    -

---
+++ pointers_inheritance/Person.cpp
@@ -0,0 +1,16 @@
+#include "Person.h"
+#include <iostream>
+using namespace std;
+
+Person::Person(string first, string last, int arbitrary) : firstname(first), lastname(last), arbitrarynumber(arbitrary)
+{
+}
+
+Person::~Person()
+{
+}
+
+string Person::GetName() const
+{
+  return firstname + " " + lastname;
+}
