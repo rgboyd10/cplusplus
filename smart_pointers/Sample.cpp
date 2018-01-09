f0af759c4aca6c1887156008ddc07858af093b51
russgbiv <rgboyd10@gmail.com>
2018-01-09 11:01:40 -0500

    -

--- smart_pointers/Sample.cpp
+++ smart_pointers/Sample.cpp
@@ -3,8 +3,37 @@
 namespace KennyKerr
 {
   template<typenmame Traits>
+
   class unique_handle
   {
+    typedef typename Traits::pointer pointer;
+    pointer m_value;
+    auto close() throw() -> void
+    {
+      if(*this)
+	{
+	  Traits::close(m_value);
+	}
+    }
+
+    public:
+    explicit unique_handle(pointer value = Traits::invalid()) throw() m_value { value }
+    {}
+    
+    ~unique_handle() throw()
+    {
+      close();
+    }
+
+    explicit operator bool() const throw()
+    {
+      return m_value != Traits::invalid();
+    }
+
+    auto get() const throw() -> pointer
+	     {
+	       
+	     }
   };
 
   struct null_handle_traits
@@ -27,5 +56,11 @@ using namespace std;
 
 auto main() -> int
 {
-  auto h = null_handle {};
+  auto raw = HANDLE { nullptr };
+  auto h = null_handle { raw };
+
+  if(h)
+    {
+    }
+
 }
