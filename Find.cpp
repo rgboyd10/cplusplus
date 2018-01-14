9cdb7b6f47069c12829fcea10764d5c24169681b
russgbiv <rgboyd10@gmail.com>
2018-01-14 12:41:12 -0500

    -

--- Find.cpp
+++ Find.cpp
@@ -17,5 +17,50 @@ int main()
     {
       weLookedFor = *result;
     }
+
+  //find the first a
+  auto letter = find(begin(s), end(s), 'a');
+  char a = *letter;
+
+  //find first odd value
+  result = find_if(begin(v), end(v), [](auto elem) {return elem % 2 != 0;});
+  weLookedFor = *result;
+
+  //find the first even value
+  result = find_if_not(begin(v), end(v), [](auto elem) {return elem % 2 != 0;});
+  weLookedFor = *result;
+
+  vector<int> primes {1,2,3,5,7,11,13};
+  result = find_first_of(begin(v), end(v), begin(primes), end(primes));
+  weLookedFor = *result;
+
+  vector<int> subsequence{2, 4};
+  result = search(begin(v), end(v), begin(subsequen ce), end(subsequence));
+  weLookedFor = *result;
+  result++;
+  result++;
+  int six = *result;
+
+  string am = "am";
+  letter = search(begin(s), end(s), begin(am), end(am));
+  a = *letter;
+
+  result = find_end(begin(v), end(v), begin(subsequence), end(subsequence));
+  result++; result++;
+  if(result != end(v))
+    {
+      weLookedFor = *result;
+    }
+
+  result = search_n(begin(v), end(v), 2, 4);
+  result--;
+  int two = *result;
+
+  result = adjacent_find(begin(v), end(v));
+  six = *result;
+  result++;
+  six = *result;
+
+  return 0;
   
 }
