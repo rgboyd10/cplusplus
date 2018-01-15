14e9e6aedbdb5afb0373f38c70ae132d1de54847
russgbiv <rgboyd10@gmail.com>
2018-01-14 23:39:35 -0500

    -

---
+++ Employee.h
@@ -0,0 +1,15 @@
+#include <string>
+
+class Employee
+{
+ public:
+ Employee(std::string first, std::string last, int sal) : firstname(first), lastname(last), salary(sal) {}
+
+  int getSalary() {return salary;}
+  std::string getSortingName() {return lastname + ", " + firstname;}
+
+ private:
+  std::string firstname;
+  std::string lastname;
+  int salary;
+};
