c780f36f8ec8d46fc871d7655c62162400b94bbd
russgbiv <rgboyd10@gmail.com>
2018-01-17 00:03:48 -0500

    -

---
+++ generating/Resource.cpp
@@ -0,0 +1,25 @@
+#include "Resource.h"
+
+int Resource::objectcount = 0;
+
+Resource::Resource()
+{
+  objectcount++;
+}
+
+Resource::Resource(const Resource & r)
+{
+  i = r.i;
+  objectcount++;
+}
+
+Resource & Resource::operator=(const Resource & r)
+{
+  i = r.i;
+  return *this;
+}
+
+Resource::~Resource()
+{
+  objectcount--;
+}
---
+++ generating/Resource.h
@@ -0,0 +1,13 @@
+class Resource
+{
+  int i = 1;
+  static int objectcount;
+
+ public:
+  Resource();
+  Resource(const Resource& r);
+  Resource& operator =(const Resource& r);
+  ~Resource();
+  void setValue(int ii) {i = ii;}
+  int getvalue() const {return i;}
+};
