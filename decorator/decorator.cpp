2c7e1c9b37c17b1a0ce9d0adc320c49547b76457
russgbiv <rgboyd10@gmail.com>
2018-01-27 22:02:32 -0500

    -

--- decorator/decorator.cpp
+++ decorator/decorator.cpp
@@ -39,13 +39,47 @@ struct Logger2
 
 template <typename Func> auto make_logger2(Func func, const string& name)
 {
-  retunr Logger2<Func>{func, name};
+  return Logger2<Func>{func, name};
 }
 
-double add(double a, double b) {return a + b;} 
+template <typename> struct Logger3;
+template <typename R, typename... Args>
+struct Logger3<R(Args...)>
+{
+  function<R(Args...)> func;
+  string name;
+
+  Logger3(const function<R(Args...)>& func, const string& name)
+    :func{func},
+     name{name}
+  {
+  }
+
+  R operator() (Args ...args)
+  {
+    cout << "Entering " << name << endl;
+    R result = func(args...);
+    cout << "Exiting " << name << endl;
+    return result;
+  }
+}; 
+
+auto make_logger3(R(*func)(Args...), const string& name)
+{
+  return Logger3<R(Args...)>
+    {
+      function<R(Args...)>(func),name
+    };
+}
+
+double add(double a, double b)
+{
+  cout << a << "+" << b << "=" << (a+b) << endl;
+  return a + b;
+} 
 int main()
 {
-  auto logged_add = make_logger3(add, "add");
+  auto logged_add = make_logger3(add, "Add");
   auto result = logged_add(2, 3);
   
   getchar();
