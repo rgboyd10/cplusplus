ab0f3744979ad4dd015e7ceb7acf022b26ba5d19
Russell <russell@localhost.localdomain>
2018-04-15 16:05:42 -0500

    -

---
+++ sum_ints.cpp
@@ -0,0 +1,41 @@
+//A program that reads and stores a series of integers and then computes the sum of the first  N integers that the user wants to sum.
+#include <iostream>
+#include <vector>
+#include <string>
+#include <sstream>
+using namespace std;
+
+int main()
+{
+  //declare variables
+  vector<int> values;
+  int userin_values;
+  int userin;
+  int sum = 0;
+  ostringstream strs;
+  string input;
+
+  //requesting the number of values to sum
+  cout << "How many values would you like to sum? " << endl;
+  cin >> userin;
+
+  //requesting the numbers to sum
+  cout << "Please enter some integers (press '|' to stop)." << endl;
+  while(cin >> userin_values)
+    {
+      
+      strs << userin_values;
+      input = strs.str();
+      if(input == "|")
+      {
+      break;
+      }
+      values.push_back(userin_values);
+    }
+        for(int x = 0; x < userin; x++)
+          {
+            sum += values[x];
+          }
+
+      cout << sum << endl;
+}
