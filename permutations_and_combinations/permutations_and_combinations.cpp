a28e7fab4f56705ff7b06339e244bff6d400c11f
russgbiv <rgboyd10@gmail.com>
2018-04-22 09:32:01 -0500

    -

--- permutations_and_combinations/permutations_and_combinations.cpp
+++ permutations_and_combinations/permutations_and_combinations.cpp
@@ -1,1 +1,13 @@
-#include
+//A program that calculates the permutations and combinations for two different numbers, and prints the result.
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+  //declare variables
+
+  //
+}
+
