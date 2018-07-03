786f596511ce90a5ce64fbe951a491264445f122
Russell Boyd <rgboyd10@gmail.com>
2018-07-03 16:42:38 -0500

    '-'

---
+++ kattis/sibice.cpp
@@ -0,0 +1,51 @@
+/*
+Young Mirko threw matches all over the floor of his room.
+
+His mom did not like that and ordered him to put all the matches in a box. Mirko soon noticed that not all of the matches on the floor fit in the box, so he decided to take the matches that don¡¯t fit and throw them in the neighbour¡¯s garbage, where his mom (hopefully) won¡¯t find them.
+
+Help Mirko determine which of the matches fit in the box his mom gave him. A match fits in the box if its entire length can lie on the bottom of the box. Mirko examines the matches one by one.
+Input
+
+The first line of input contains an integer N(1¡ÜN¡Ü50), the number of matches on the floor, and two integers W and H, the dimensions of the box (1¡ÜW¡Ü100, 1¡ÜH¡Ü100).
+
+Each of the following N lines contains a single integer between 1 and 1000 (inclusive), the length of one match.
+Output
+
+For each match, in the order they were given in the input, output on a separate line ¡°DA¡± if the match fits in the box or ¡°NE¡± if it does not.
+*/
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+  //declare vars
+  vector <int> match_length;
+  int lengths;
+  int number_of_matches = 0;
+  int width;
+  int height;
+
+  //read in values
+  cin >> number_of_matches >> width >> height;
+  while(cin >> lengths && number_of_matches <= 50)
+    {
+      match_length.push_back(lengths);
+      number_of_matches++;
+    }
+
+  //write the number of matches, width, and height
+  cout << number_of_matches << " " << width << " " << height << endl;
+
+  //compute and write the lengths and DA or NE
+  for(int x = 0; x < match_length.length(); x++)
+    {
+      if(Math.sqrt(Math.pow(width,2) + Math.pow(height,2)) >= match_length[x])
+	{
+	  cout << match_length[x] << " DA " << endl;
+	}
+      else
+	cout << match_length[x] << " NE " << endl;
+    }
+}
