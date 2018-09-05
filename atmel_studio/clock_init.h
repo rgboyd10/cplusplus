b680429a213000acbe61cee2680f019ac13f24e8
russgbiv <rgboyd10@gmail.com>
2018-09-04 20:44:19 -0500

    '-'

---
+++ atmel_studio/clock_init.h
@@ -0,0 +1,15 @@
+/*******************************************************
+System clock initialization created by the
+CodeWizardAVR V3.32 Automatic Program Generator
+© Copyright 1998-2017 Pavel Haiduc, HP InfoTech s.r.l.
+http://www.hpinfotech.com
+
+Project : 
+*******************************************************/
+
+#ifndef _CLOCK_INIT_INCLUDED_
+#define _CLOCK_INIT_INCLUDED_
+
+void system_clocks_init(void);
+
+#endif
---
+++ atmel_studio/timers_init.h
@@ -0,0 +1,18 @@
+/*******************************************************
+Timers/Counters initialization created by the
+CodeWizardAVR V3.32 Automatic Program Generator
+© Copyright 1998-2017 Pavel Haiduc, HP InfoTech s.r.l.
+http://www.hpinfotech.com
+
+Project : 
+*******************************************************/
+
+#ifndef _TIMERS_INIT_INCLUDED_
+#define _TIMERS_INIT_INCLUDED_
+
+// Disable a Timer/Counter type TC0
+void tc0_disable(TC0_t *ptc);
+// Timer/Counter TCC0 initialization
+void tcc0_init(void);
+
+#endif
