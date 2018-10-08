e32e88237161807082beda7237bb6e4191969373
root <root@localhost.localdomain>
2018-10-08 08:33:36 -0500

    -

---
+++ hackerrank/io.cpp
@@ -0,0 +1,12 @@
+#include <cmath>
+#include <iostream>
+using namespace std;
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int x, y, z, sum;
+    cin >> x >> y >> z;
+    sum = x + y + z;
+    cout << sum;
+    return 0;
+}
