cbc865630a149d1addec5de8f9214fd727b6e23d
rboyd <russellboyd@rei-rboyd.rei.local>
2018-11-20 16:58:25 -0600

    -

--- OpenMP/OpenMP/OpenMP/OpenMP.cpp
+++ OpenMP/OpenMP/OpenMP/OpenMP.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <chrono>
 #include <cstdio>
+#include <vector>
 using namespace std;
 #include <omp.h>
 
@@ -75,12 +76,44 @@ void single_master()
 		}
 	}
 }
+
+void sync()
+{
+	printf("\nATOMIC\n");
+	int sum = 0;
+#pragma omp parallel for num_threads(128)
+	{
+		for (int i = 0; i < 100; i++)
+		{
+#pragma omp atomic
+			++sum;
+		}
+
+		cout << sum;
+		printf("\nORDERED\n");
+		vector<int> squares;
+
+#pragma omp parallel for
+		{
+			for (int i = 0; i < 20; i++)
+			{
+				printf("%d : %d\t", omp_get_thread_num(), i);
+				int j = i * i;
+				squares.push_back(j);
+			}
+
+			printf("\n");
+			for (auto v : squares) printf("%d\t", v);
+		}
+	}
+}
 int main(int argc, char* argv[])
 {
 	//hello_openmp();
 	//pfor();
 	//sections();
-	single_master();
+	//single_master();
+	sync();
 	getchar();
 	return 0;
 }
