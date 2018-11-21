4f0c2b6b0a3cef42fa295e1f150b9b8647314e6c
rboyd <russellboyd@rei-rboyd.rei.local>
2018-11-21 16:59:14 -0600

    -

--- MPI/MPI/MPI/MPI.cpp
+++ MPI/MPI/MPI/MPI.cpp
@@ -46,6 +46,20 @@ int main(int argc, char* argv[])
 		MPI_Get_count(&status, MPI_CHAR, &count);
 		cout << "we have " << count;
 
+		char * buffer = new char[count];
+
+		MPI_Recv(&buffer, count, MPI_CHAR, 0, 0, MPI_COMM_WORLD, &status);
+		cout << "we got " << buffer << endl;
+		
+		istringstream iss(buffer);
+		boost::archive::text_iarchive ar(iss);
+		Person p;
+
+		ar >> p;
+
+		cout << "Hello, " << p.name << ", you are " << p.age << " year old!" << endl;
+		delete[] buffer;
+
 
 	}
 
